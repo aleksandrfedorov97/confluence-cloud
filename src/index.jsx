cc3d9073a6b3878a959ac97819bd1938881bef01
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-10-12 22:48:40 +0300

    init commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import ForgeUI, { render, Fragment, Macro, Text } from "@forge/ui";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <Macro
+    app={<App />}
+  />
+);
+// test-dev-oo.atlassian.net
